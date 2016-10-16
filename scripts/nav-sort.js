91452ce8b878766af6d73ee45dc5ea415b0d2569
Ryan "archaen" Pando <vretha@gmail.com>
2016-10-15 18:51:06 -0600

    Sort by drop down working.

---
+++ scripts/nav-sort.js
@@ -0,0 +1,23 @@
+(function () {
+  var app = angular.module('redditize'); //jshint ignore:line
+
+  app.controller('sortController', function($rootScope) {
+    $rootScope.sortString = '';
+    $rootScope.orderString = '-rating';
+    this.selectValue = 'by Votes';
+
+    this. setVotes = () => {
+      this.selectValue = 'by Votes';
+      $rootScope.orderString = '-rating';
+    };
+    this. setDate = () => {
+      this.selectValue = 'by Date';
+      $rootScope.orderString = '-timeStamp';
+    };
+    this. setAlpha = () => {
+      this.selectValue = 'Alphabetically';
+      $rootScope.orderString = 'title';
+    };
+
+  });
+})();
